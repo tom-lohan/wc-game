f2d97ba4b230d81bfcd144f2388fe2ac4d4c6704
Tom Lohan <t.lohan.projects@gmail.com>
2022-07-24 19:10:54 +0100

    updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Card } from 'antd';
 import React from 'react';
 import './App.css';
+import "antd/dist/antd.css";
 import { counties } from './data/counties';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <main className='counties'>
         {counties.map(country => (
-           <Card size="small" title={`${country.name} ${country.emoji}`} style={{ width: 300 }}>
+           <Card size="small" title={`${country.name} ${country.emoji}`} className="countryCard">
            <p>Group: {country.group}</p>
          </Card>
         ))}
+        </main>
         </header>
     </div>
   );
