c5b0b0993a445c223c2bdad1ab17fbfa26eb5206
Tom Lohan <t.lohan.projects@gmail.com>
2022-07-24 21:58:40 +0100

    remove antd and use tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import { Card } from 'antd';
-import React from 'react';
-import './App.css';
-import "antd/dist/antd.css";
-import { counties } from './data/counties';
+import React from 'react'
+import './App.css'
+import { counties } from './data/counties'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <main className='counties'>
-        {counties.map(country => (
-           <Card size="small" title={`${country.name} ${country.emoji}`} className="countryCard">
-           <p>Group: {country.group}</p>
-         </Card>
-        ))}
-        </main>
-        </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className="text-center text-3xl font-bold underline p-5">
+                    Hello world!
+                </h1>
+                <main className="flex flex-wrap gap-4">
+                    {counties.map((country) => (
+                        <div className="m-3 p-3 border border-zinc-400 rounded">
+                            <p>Name: {country.name}</p>
+                            <p>Group: {country.group}</p>
+                        </div>
+                    ))}
+                </main>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
