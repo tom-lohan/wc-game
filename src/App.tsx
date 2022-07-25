a1e8db15b075112fe2e01e091d3ef4afbaea6452
Tom Lohan <t.lohan.projects@gmail.com>
2022-07-25 21:07:41 +0100

    demo github actions

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,18 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1 className="text-center text-3xl font-bold underline p-5">
+                <h1
+                    data-testid="hello-world"
+                    className="text-center text-3xl font-bold underline p-5"
+                >
                     Hello world!
                 </h1>
                 <main className="flex flex-wrap gap-4">
-                    {counties.map((country) => (
-                        <div className="m-3 p-3 border border-zinc-400 rounded">
+                    {counties.map((country, index) => (
+                        <div
+                            key={index}
+                            className="m-3 p-3 border border-zinc-400 rounded"
+                        >
                             <p>Name: {country.name}</p>
                             <p>Group: {country.group}</p>
                         </div>
