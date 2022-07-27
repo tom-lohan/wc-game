afcb02b1a8c7404e188e8a36fb84346245a24f83
Tom Lohan <t.lohan.projects@gmail.com>
2022-07-27 22:32:15 +0100

    added basic add player functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import { LandingPage } from './components'
+import { AddPlayersPage, LandingPage } from './components'
 import 'antd/dist/antd.min.css'
 
+export enum PageType {
+    LANDING_PAGE = 'landing_page',
+    ADD_PLAYERS_PAGE = 'add_players_page',
+}
+
 function App() {
+    const [selectedPage, setSelectedPage] = useState<PageType>(
+        PageType.LANDING_PAGE
+    )
+
     return (
         <div className="App">
-            <LandingPage />
+            {selectedPage === PageType.LANDING_PAGE && (
+                <LandingPage
+                    clickHandler={() =>
+                        setSelectedPage(PageType.ADD_PLAYERS_PAGE)
+                    }
+                />
+            )}
+            {selectedPage === PageType.ADD_PLAYERS_PAGE && <AddPlayersPage />}
         </div>
     )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { LandingPage } from './landingPage';
+export { AddPlayersPage } from './addPlayersPage';
